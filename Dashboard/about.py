1d96fed51a9c27d7e7b116cfb42f084f2a45d96c
Fakhrus Syakir <161460143+KHR00S@users.noreply.github.com>
2025-07-04 15:48:28 +0700

    p

--- Dashboard/about.py
+++ Dashboard/about.py
@@ -49,88 +49,8 @@ def app():
         # Model Section
         st.header("Model yang Digunakan")
         st.markdown("""
-            ### Model Forecast (Seasonal VARMA dan Vector Autoregresive)
-            Kami menggunakan dua model Seasonal VARMA dan Vector Autoregresive untuk meramalkan indikator iklim pada berbagai interval waktu
-        """)
-        """st.write('#### Seasonal VARMA')
-        st.latex(r'\Phi(B^s)\phi(B)Y_t = \Theta(B^S)\theta(B)\alpha_t')
-
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑌𝑡        : vektor deret waktu pada periode ke-t,
-        - B          : operator pembeda,
-        - 𝛼𝑡        : vektor residual white-noise,
-        - 𝜙𝑝(𝐵)     : matriks polinomial AR,
-        - 𝑞(𝐵)      : matriks polinomial MA,
-        - Φ𝑃(𝐵)𝑠    : matriks polinomial AR musiman dan 
-        - Θ𝑄(𝐵)𝑠    : matriks polinomial MA musiman
-        """"""
-
-        st.write('#### Vector Autoregressive (VAR)')
-        st.latex(r'\Phi(B^s)\phi(B)Y_t = \Theta(B^S)\theta(B)\alpha_t')
-
-        st.markdown("""
-        """###### Keterangan:
-        - 𝑌𝑡      : vektor dari variabel endogen pada waktu t
-        - 𝐴𝑖      : matriks koefisien untuk lag i
-        - 𝑝       : orde dari model VAR
-        - 𝛼𝑡      : vektor residual white-noise
-        """
-        
-        """st.markdown("""
-            ### Model Klasifikasi Support Vector Machine (SVM)
-            #Kami menggunakan model Klasifikasi Support Vector Machine (SVM) untuk mengklasifikasikan pottensi Upwelling
-        """)
-        
-        st.write('#### Kernel Linear')
-        # Linear kernel formula
-        st.latex(r'''
-                    K(x_i, x) = x_i^T x
-                    ''')
-        
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑥𝑖 = Vektor data training
-        - 𝑥 = Vektor data testing
-        """
-        
-        """st.write('#### Kernel RBF')
-        # Linear kernel formula
-        st.latex(r'''
-                    K(x_i, x) = \exp\left(- \gamma \left| x_i^T x \right|^2 \right)
-                    ''')
-        
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑥𝑖 = Vektor data training
-        - 𝑥 = Vektor data testing
-        - 𝛾= gamma
-        """
-        
-        """st.write('#### Kernel Polynomial')
-        # Linear kernel formula
-        st.latex(r'''
-                    K(x_i, x) = (\gamma \cdot x_i^T x + r)^p
-                    ''')
-        
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑥𝑖 = Vektor data training
-        - 𝑥 = Vektor data testing
-        - 𝛾= gamma
-        - 𝑟= coef ()
-        - 𝑝 = degree
-        """
-
-
-        # Evaluation Metrics
-        st.write('### Metrik Evaluasi')
-        st.markdown("""
-            - Indeks Kejernihan Langit   : MSE = 0,0526, RMSE = 0,2295, MAE = 0,1815
-            - Suhu Pada Ketinggian 2 M   : MSE = 0,0341, RMSE = 0,1847, MAE = 0,1514
-            - Curah Hujan                : MSE = 0,0738, RMSE = 0,2721, MAE = 0,2191
-            - Tekanan Permukaan          : MSE = 0,0399, RMSE = 0,1998, MAE = 0,1602
-            - Kecepatan Angin            : MSE = 0,0287, RMSE = 0,1697, MAE = 0,1347
+            ### Model Forecast ( Vector Autoregresive)
+            Kami menggunakan Vector Autoregresive untuk meramalkan indikator iklim pada berbagai interval waktu
         """)
 
         # Tools and Technologies
@@ -162,33 +82,6 @@ def app():
         st.markdown("""
             Jika Anda memiliki pertanyaan atau umpan balik, silakan hubungi kami:
         """)
-        col_4, col_5 = st.columns(2)
-        with col_4:
-            st.markdown("""
-                - ##### Muhammad Zia Ulhaq
-                                """)
-            st.image('zz.png')
-            st.markdown("""
-                    - Email   : [ziaswatfbicia@gmail.com](mailto:ziaswatfbicia@gmail.com) 
-                    - LinkedIn: [Muhammad Zia Ulhaq](https://www.linkedin.com/in/muhammad-zia-ulhaq-8373112b9/) 
-                                    """)
-            st.markdown("""
-                - ##### Zahra Ifma Aziza
-                                """)
-            st.image('z.png')
-            st.markdown("""
-                    - Email   : [zahraifmaa@gmail.com](mailto:zahraifmaa@gmail.com) 
-                    - LinkedIn: [Zahra Ifma Aziza](https://www.linkedin.com/in/zahra-ifma-aziza/) 
-                    """)
-            st.markdown("""
-                - ##### Muhammad Farid
-                                """)
-            st.image('f.png')
-            st.markdown("""
-                    - Email   : [mhdfaridz93@gmail.com](mailto:mhdfaridz93@gmail.com) 
-                    - LinkedIn: [Muhammad Farid](https://www.linkedin.com/in/mhd-farid/)
-            """)
-        with col_5:
             st.markdown("""
                 - ##### Fakhrus Syakir
                 """)
@@ -197,25 +90,6 @@ def app():
                     - Email   : [fakhroosyakir@gmail.com](mailto:fakhroosyakir@gmail.com) 
                     - LinkedIn: [Fakhrus Syakir](https://www.linkedin.com/in/fakhrus-syakir-65bb72205/) 
                     """)
-                    
-            st.markdown("""
-                - ##### Teuku Muhammad Faiz Nuzullah
-                """)
-            st.image('p.png')
-            st.markdown("""
-                    - Email   : [faiznuzullah@gmail.com](mailto:faiznuzullah@gmail.com) 
-                    - LinkedIn: [Teuku Muhammad Faiz Nuzullah](https://www.linkedin.com/in/teuku-muhammad-f-4a906a239) 
-                    """)
-            st.markdown("""
-                - #####  Novi Reandy Sasmita, S.Si., M.Sc.
-                """)
-            st.image('g.png', use_column_width=True)
-            st.markdown("""
-                    - Email   : [novireandys@usk.ac.id](mailto:novireandys@usk.ac.id) 
-                    - LinkedIn: [Novi Reandy Sasmita](https://www.linkedin.com/in/novi-reandy-sasmita-93371a72/)
-                    - Website : [Novi Reandy Sasmita's Website](https://www.reandy.info)
-            """)
-
 
     else:
         # English content
@@ -260,87 +134,8 @@ def app():
         # Model Section
         st.header("Models Used")
         st.markdown("""
-            #### Forecasting Models (Seasonal VARMA and Vector Autoregressive)
-            We use two models, Seasonal VARMA and Vector Autoregressive, to forecast climate indicators at various time intervals:
-        """)
-        """st.write('#### Seasonal VARMA')
-        st.latex(r'\Phi(B^s)\phi(B)Y_t = \Theta(B^S)\theta(B)\alpha_t')
-
-        st.markdown(""" 
-        """###### Explanation:
-        - 𝑌𝑡        : vector of time series at time t,
-        - B          : difference operator,
-        - 𝛼𝑡        : vector of white-noise residuals,
-        - 𝜙𝑝(𝐵)     : AR polynomial matrix,
-        - 𝑞(𝐵)      : MA polynomial matrix,
-        - Φ𝑃(𝐵)𝑠    : seasonal AR polynomial matrix
-        - Θ𝑄(𝐵)𝑠    : seasonal MA polynomial matrix
-        """
-
-        """st.write('#### Vector Autoregressive (VAR)')
-        st.latex(r'\Phi(B^s)\phi(B)Y_t = \Theta(B^S)\theta(B)\alpha_t')
-
-        st.markdown("""
-        """###### Explanation:
-        - 𝑌𝑡      : vector of endogenous variables at time t
-        - 𝐴𝑖      : coefficient matrix for lag i
-        - 𝑝       : order of the VAR model
-        - 𝛼𝑡      : vector of white-noise residuals
-        """
-        
-        """st.markdown("""
-            ### Model Klasifikasi Support Vector Machine (SVM)
-            #We use Classification model Support Vector Machine (SVM) for classified Upwelling potensial
-        """)
-        
-        st.write('#### Kernel Linear')
-        # Linear kernel formula
-        st.latex(r'''
-                    K(x_i, x) = x_i^T x
-                    ''')
-        
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑥𝑖 = Vektor data training
-        - 𝑥 = Vektor data testing
-        """
-        
-        """st.write('#### Kernel RBF')
-        # Linear kernel formula
-        st.latex(r'''
-                    K(x_i, x) = \exp\left(- \gamma \left| x_i^T x \right|^2 \right)
-                    ''')
-        
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑥𝑖 = Vektor data training
-        - 𝑥 = Vektor data testing
-        - 𝛾= gamma
-        """
-        
-        """st.write('#### Kernel Polynomial')
-        # Linear kernel formula
-        st.latex(r'''
-                    K(x_i, x) = (\gamma \cdot x_i^T x + r)^p
-                    ''')
-        
-        st.markdown(""" 
-        """###### Keterangan:
-        - 𝑥𝑖 = Vektor data training
-        - 𝑥 = Vektor data testing
-        - 𝛾= gamma
-        - 𝑟= coef ()
-        - 𝑝 = degree
-        """
-
-        # Evaluation Metrics
-        st.write('### Evaluation Metrics')
-        st.markdown("""
-            - All Sky Insolation Clearness Index: MSE = 0,0526, RMSE = 0,2295, MAE = 0,1815
-            - Temperature at 2 M                 : MSE = 0,0341, RMSE = 0,1847, MAE = 0,1514
-            - Precipitation                      : MSE = 0,0738, RMSE = 0,2721, MAE = 0,2191
-            - Surface Pressure                   : MSE = 0,0399, RMSE = 0,1998, MAE = 0,1602
-            - Wind Speed                         : MSE = 0,0287, RMSE = 0,1697, MAE = 0,1347
+            #### Forecas Vector Autoregressive)
+            We use Vector Autoregressive, to forecast climate indicators at various time intervals:
         """)
 
         # Tools and Technologies
@@ -372,33 +167,6 @@ def app():
         st.markdown("""
             If you have any questions or feedback, please contact us:
         """)
-        col_4, col_5 = st.columns(2)
-        with col_4:
-            st.markdown("""
-                - ##### Muhammad Zia Ulhaq
-                                """)
-            st.image('zz.png')
-            st.markdown("""
-                    - Email   : [ziaswatfbicia@gmail.com](mailto:ziaswatfbicia@gmail.com) 
-                    - LinkedIn: [Muhammad Zia Ulhaq](https://www.linkedin.com/in/muhammad-zia-ulhaq-8373112b9/) 
-                                    """)
-            st.markdown("""
-                - ##### Zahra Ifma Aziza
-                                """)
-            st.image('z.png')
-            st.markdown("""
-                    - Email   : [zahraifmaa@gmail.com](mailto:zahraifmaa@gmail.com) 
-                    - LinkedIn: [Zahra Ifma Aziza](https://www.linkedin.com/in/zahra-ifma-aziza/) 
-                    """)
-            st.markdown("""
-                - ##### Muhammad Farid
-                                """)
-            st.image('f.png')
-            st.markdown("""
-                    - Email   : [mhdfaridz93@gmail.com](mailto:mhdfaridz93@gmail.com) 
-                    - LinkedIn: [Muhammad Farid](https://www.linkedin.com/in/mhd-farid/)
-            """)
-        with col_5:
             st.markdown("""
                 - ##### Fakhrus Syakir
                 """)
@@ -407,21 +175,3 @@ def app():
                     - Email   : [fakhroosyakir@gmail.com](mailto:fakhroosyakir@gmail.com) 
                     - LinkedIn: [Fakhrus Syakir](https://www.linkedin.com/in/fakhrus-syakir-65bb72205/) 
                     """)
-                    
-            st.markdown("""
-                - ##### Teuku Muhammad Faiz Nuzullah
-                """)
-            st.image('p.png')
-            st.markdown("""
-                    - Email   : [faiznuzullah@gmail.com](mailto:faiznuzullah@gmail.com) 
-                    - LinkedIn: [Teuku Muhammad Faiz Nuzullah](https://www.linkedin.com/in/teuku-muhammad-f-4a906a239) 
-                    """)
-            st.markdown("""
-                - #####  Novi Reandy Sasmita, S.Si., M.Sc.
-                """)
-            st.image('g.png', use_column_width=True)
-            st.markdown("""
-                    - Email   : [novireandys@usk.ac.id](mailto:novireandys@usk.ac.id) 
-                    - LinkedIn: [Novi Reandy Sasmita](https://www.linkedin.com/in/novi-reandy-sasmita-93371a72/)
-                    - Website : [Novi Reandy Sasmita's Website](https://www.reandy.info)
-            """)
